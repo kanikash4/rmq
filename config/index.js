248e5c30916940e755bf083566d75f9de5353a56
Kanika Sharma <kanikash4@gmail.com>
2017-12-06 18:32:22 +0530

    Config Additions

--- config/index.js
+++ config/index.js
@@ -3,12 +3,35 @@
 
 var rmqCOnfig = {
 	ENVIRONMENT : process.env.NODE_ENV || 'development',
+  COMMON : {
+    QUIET_MODE : true,
+    }
 	development : {
 		RABBITMQ : {
-			CONNECT_STRING : 'amqp://guest:guest@localhost:5672?heartbeat=60',
+			CLOUDAMQP_URL : 'amqp://guest:guest@localhost:5672?heartbeat=60',
+			RETRY : 5000
+		}
+	},
+	production : {
+		RABBITMQ : {
+			CLOUDAMQP_URL : 'amqp://guest:guest@localhost:5672?heartbeat=60',
 			RETRY : 5000 //milliseconds
 		}
 	}
 };
 
-module.exports =  rmqCOnfig;
+var loadConfig = function() {
+	var env = rmqCOnfig.ENVIRONMENT,
+	    loadedConfig = config.COMMON;
+
+	if(["development", "production"].indexOf(env) <= -1) {
+		env = "development";
+	}
+
+	Object.keys(rmqCOnfig[env]).forEach(function(key) {
+		loadedConfig[key] = config[env][key];
+	});
+	return loadedConfig;
+};
+
+module.exports = loadConfig();
